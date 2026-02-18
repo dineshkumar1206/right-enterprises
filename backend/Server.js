0024035030703d9e4fde2b54f9eb177dbfcf0ddd
dinesh <dineshkumarp1206@gmail.com>
2026-02-18 17:58:51 +0530

    update email backend

--- backend/Server.js
+++ backend/Server.js
@@ -40,17 +40,19 @@ app.post("/contact", async (req, res) => {
     const encryptedEmail = encrypt(email);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       replyTo: email,
-      to: process.env.EMAIL_USER,
+      to: process.env.ADMIN_EMAIL,
       subject: "New Contact Form Submission",
       html: `
         <h3>New Contact Form</h3>
